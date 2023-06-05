c29c8496b853b07fc13c23ab5f019ba8b323042e
Metehan <metehan.bolat.ie@gmail.com>
2023-06-06 01:02:45 +0300

    Model

---
+++ core/domain/src/main/java/com/metehanbolat/domain/model/CharacterItem.kt
@@ -0,0 +1,8 @@
+package com.metehanbolat.domain.model
+
+data class CharacterItem(
+    val name: String? = null,
+    val status: String? = null,
+    val species: String? = null,
+    val gender: String? = null
+)
