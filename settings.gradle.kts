090f5d4b0a0da65ad5a677aff77b04870c4bc17a
Metehan <metehan.bolat.ie@gmail.com>
2023-06-05 23:56:37 +0300

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "RickAndMortyCompose"
+include(":app")
+include(":core")
+include(":core:data")
+include(":core:domain")
+include(":feature")
